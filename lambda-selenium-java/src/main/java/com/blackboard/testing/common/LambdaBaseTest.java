f26e3716c848bf548d99015da6fb1a26bd975823
wesmcouch <wesmcouch@gmail.com>
2017-10-13 12:21:57 -0400

    Add screenshot capability to base test

--- lambda-selenium-java/src/main/java/com/blackboard/testing/common/LambdaBaseTest.java
+++ lambda-selenium-java/src/main/java/com/blackboard/testing/common/LambdaBaseTest.java
@@ -1,22 +1,34 @@
 package com.blackboard.testing.common;
 
 import com.blackboard.testing.driver.LambdaWebDriverThreadLocalContainer;
+import com.blackboard.testing.lambda.LambdaTestHandler;
 import com.blackboard.testing.testcontext.EnvironmentDetector;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
-import com.gargoylesoftware.htmlunit.javascript.configuration.BrowserName;
 import org.junit.BeforeClass;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public abstract class LambdaBaseTest {
 
     @BeforeClass
     public static void baseTestBeforeClass() {
-        Configuration.browser = BrowserName.CHROME.toString();
+        Configuration.browser = "chrome";
         Configuration.reopenBrowserOnFail = false;
 
         if (EnvironmentDetector.inLambda()) {
-            Configuration.reportsFolder = "/tmp/selenidereports";
             WebDriverRunner.webdriverContainer = new LambdaWebDriverThreadLocalContainer();
+        }
+    }
+
+    protected void screenshot(String description) {
+        if (EnvironmentDetector.inLambda()) {
+            LambdaTestHandler.addAttachment(description + ".png",
+                    ((TakesScreenshot) WebDriverRunner.getWebDriver())
+                            .getScreenshotAs(OutputType.BYTES));
+        } else {
+            Selenide.screenshot(description);
         }
     }
 }
